ff694529248699c59d991022108530247a92d56d
PatrickAlphac <54278053+PatrickAlphaC@users.noreply.github.com>
2023-10-25 12:11:43 -0400

    make it worse lol

--- script/DeployPasswordStore.s.sol
+++ script/DeployPasswordStore.s.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.18;
 
-import {Script, console2} from "forge-std/Script.sol";
+import {Script} from "forge-std/Script.sol";
 import {PasswordStore} from "../src/PasswordStore.sol";
 
 contract DeployPasswordStore is Script {
